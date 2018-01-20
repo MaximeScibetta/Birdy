4664467667cca565c7780d716989a63df88bab53
Maxime Scibetta <maxime.scibetta@outlook.com>
2018-01-20 16:29:43 +0100

    Fixed with real disconection

--- app/components/screens/Profile.js
+++ app/components/screens/Profile.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet,Button } from 'react-native'
 import { connect } from 'react-redux'
-import { Actions } from 'react-native-router-flux' 
+import { Actions } from 'react-native-router-flux'
+import firebase from 'firebase';
 
-const Profile = () => {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.welcome}>Mon profile</Text>
-            <Button
-                onPress={Actions.login}
-                title='Se deconnecter'>
-            </Button>
-        </View>
-    )
+class Profile extends Component {
+    onButtonPress(){
+        firebase.auth().signOut()
+            .then(Actions.login)
+     }
+
+     render(){
+         return (
+             <View style={styles.container}>
+                 <Text style={styles.welcome}>Mon profile</Text>
+                 <Button
+                     onPress={this.onButtonPress}
+                     title='Se deconnecter'>
+                 </Button>
+             </View>
+         )
+     }
 }
 
 
