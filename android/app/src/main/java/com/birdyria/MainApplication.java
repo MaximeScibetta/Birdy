d080ab36860e92721c27689f83e6028c21c30599
Maxime Scibetta <maxime.scibetta@outlook.com>
2018-01-21 14:36:33 +0100

    AirMap component does not exist

--- android/app/src/main/java/com/birdyria/MainApplication.java
+++ android/app/src/main/java/com/birdyria/MainApplication.java
@@ -2,12 +2,12 @@ package com.birdyria;
 
 import android.app.Application;
 
+import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.airbnb.android.react.maps.MapsPackage;
 
 import java.util.Arrays;
 import java.util.List;
--- app/components/Home.js
+++ app/components/Home.js
@@ -6,33 +6,66 @@ import {
 } from 'react-native';
 import { connect } from 'react-redux';
 import { Actions } from 'react-native-router-flux';
-import FloatingActionButton from 'react-native-action-button';
+//import FloatingActionButton from 'react-native-action-button';
+import MapView from 'react-native-maps';
+import PropTypes from 'prop-types';
 
-const Home = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Home</Text>
-      <FloatingActionButton
+
+class Home extends Component {
+  static propTypes = {
+    routes: PropTypes.object,
+  };
+  // const { region } = this.props;
+  // console.log(region);
+  render() {
+    const { routes } = this.context;
+    return (
+      <View style={styles.container}>
+        {/* <Text style={styles.welcome}>Home</Text> */}
+        {/* <FloatingActionButton
         hideShadow={true} // this is to avoid a bug in the FAB library.
         buttonColor="rgba(231,76,60,1)"
-        onPress={Actions.login} />
-    </View>
-  )
+        onPress={Actions.login} /> */}
+        <MapView
+          style={styles.map}
+          region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+        >
+        </MapView>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
+  // container: {
+  //   flex: 1,
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  //   backgroundColor: '#bb0000',
+  // },
+  // welcome: {
+  //   fontSize: 20,
+  //   textAlign: 'center',
+  //   margin: 10,
+  //   color: '#ffffff',
+  // },
   container: {
-    flex: 1,
-    justifyContent: 'center',
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    backgroundColor: '#bb0000',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-    color: '#ffffff',
-  }
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
 })
 
-export default connect(({routes}) => ({routes}))(Home)
+export default connect(state => ({
+  routes: state.routes
+}), null)(Home);
