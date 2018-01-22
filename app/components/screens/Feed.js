a7f960deecc9104d1a4c61279e73bc9fdfa9e9e2
Maxime Scibetta <maxime.scibetta@outlook.com>
2018-01-22 19:00:52 +0100

    Stock all capture in feed state from firebase

--- app/components/screens/Feed.js
+++ app/components/screens/Feed.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native'
 import { connect } from 'react-redux'
 import { Actions } from 'react-native-router-flux'
+import firebase from 'firebase';
+
 
 class Feed extends Component{
 
+    componentWillMount(){
+
+        firebase.database().ref("captures/")
+            .on("value", (snapshot) => {
+                this.setState(snapshot.val())
+            });
+            
+    }
+
     render(){
+        console.log(this.state)
         return (
             <View style={styles.container}>
                 <Text style={styles.welcome}>Fil d'actualité</Text>
