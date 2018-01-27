40ecc818beb795c1a02dc6f4bca3517a7adb8352
Maxime Scibetta <maxime.scibetta@outlook.com>
2018-01-27 14:28:13 +0100

    Designed AddBirdForm

--- app/components/common/AddBirdForm.js
+++ app/components/common/AddBirdForm.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { ScrollView, View, Text, Button, Picker, Image, TouchableWithoutFeedback } from 'react-native';
+import { ScrollView, View, Text, Picker, Image, TouchableWithoutFeedback, StyleSheet } from 'react-native';
 import { Field, Spinner } from './';
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
 import DatePicker from 'react-native-datepicker'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { RadioButtons, SegmentedControls } from 'react-native-radio-buttons'
+import { FormLabel, FormInput, Button } from 'react-native-elements'
 import nanoid from 'nanoid';
+
 
 class AddBirdForm extends Component {
     state = {
@@ -37,7 +39,7 @@ class AddBirdForm extends Component {
 
         const { userData, how, date, where, latin_name, ring_nbr, ring_nbr_series, length, lvl, sexe, years } = this.state;
         const captureId = nanoid();
-       
+
         firebase.database().ref('captures/' + captureId).set({
             id: captureId,
             user: userData,
@@ -68,7 +70,6 @@ class AddBirdForm extends Component {
 
         function renderOption(option, selected, onSelect, index) {
             const style = selected ? { color: '#448aff', fontWeight: 'bold' } : {};
-
             return (
                 <TouchableWithoutFeedback onPress={onSelect} key={index}>
                     <View>
@@ -84,7 +85,7 @@ class AddBirdForm extends Component {
         console.log(this.state)
         return (
             <ScrollView>
-                <View>
+                <View style={styles.marginItem}>
                     <Text>Comment l'oiseau a-t-il été capturé </Text>
                     <Picker
                         mode="dropdown"
@@ -96,10 +97,10 @@ class AddBirdForm extends Component {
                         <Picker.Item label="A la cânne à pèche" value="canne" />
                     </Picker>
                 </View>
-                <View>
+                <View style={styles.marginItem}>
                     <Text>Quand l'oiseau a-t-il été capturé </Text>
                     <DatePicker
-                        style={{ width: 200 }}
+                        style={{ width: 200, marginTop: 10 }}
                         date={this.state.date}
                         mode="date"
                         placeholder="Sélectionnez une date"
@@ -123,8 +124,9 @@ class AddBirdForm extends Component {
                         onDateChange={(date) => { this.setState({ date: date }) }}
                     />
                 </View>
-                <View>
-                    <Text>Où l'oiseau a-t-il été capturé</Text>
+                <View style={styles.marginItem}>
+                    <Text
+                        style={{ marginBottom: 10 }}>Où l'oiseau a-t-il été capturé</Text>
                     <GooglePlacesAutocomplete
                         placeholder='Rechercher le lieu'
                         minLength={2} // minimum length of text to search
@@ -171,38 +173,56 @@ class AddBirdForm extends Component {
                         debounce={0} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
                     />
                 </View>
-                <Field
-                    keyboardType='default'
-                    label='Nom latin'
-                    placeholder='rougus gorus'
-                    value={this.state.latin_name}
-                    onChangeText={text => this.setState({ latin_name: text })} />
-                <Field
-                    keyboardType='default'
-                    label='N° de la bague'
-                    placeholder='XD428DX187D'
-                    value={this.state.ring_nbr}
-                    onChangeText={text => this.setState({ ring_nbr: text })} />
-                <Field
-                    keyboardType='numeric'
-                    label='N° de série de la bague'
-                    placeholder='85186245'
-                    value={this.state.ring_nbr_series}
-                    onChangeText={text => this.setState({ ring_nbr_series: text })} />
-                <Field
-                    keyboardType='numeric'
-                    label='Longueur alair'
-                    placeholder='235mm'
-                    value={this.state.length}
-                    onChangeText={text => this.setState({ length: text })} />
-                <Field
-                    keyboardType='numeric'
-                    label='Niveau de graisse'
-                    placeholder='0.52%'
-                    value={this.state.lvl}
-                    onChangeText={text => this.setState({ lvl: text })} />
                 <View>
-                    <Text>Sexe</Text>
+                    <FormLabel>Nom latin</FormLabel>
+                    <FormInput
+                        keyboardType='default'
+                        placeholder='rougus gorus'
+                        value={this.state.latin_name}
+                        onChangeText={text => this.setState({ latin_name: text })} />
+                </View>
+                <View>
+                    <FormLabel>N° de la bague</FormLabel>
+                    <FormInput
+                        keyboardType='default'
+                        placeholder='XD428DX187D'
+                        value={this.state.ring_nbr}
+                        onChangeText={text => this.setState({ ring_nbr: text })}/>
+                </View>
+                <View>
+                    <FormLabel>N° de série de la bague</FormLabel>
+                    <FormInput
+                        keyboardType='numeric'
+                        placeholder='85186245'
+                        value={this.state.ring_nbr_series}
+                        onChangeText={text => this.setState({ ring_nbr_series: text })} />
+                </View>
+                <View>
+                    <FormLabel>Longueur alair</FormLabel>
+                    <FormInput
+                        keyboardType='numeric'
+                        placeholder='235mm'
+                        value={this.state.length}
+                        onChangeText={text => this.setState({ length: text })} />
+                </View>
+                <View>
+                    <FormLabel>Niveau de graisse</FormLabel>
+                    <FormInput
+                        keyboardType='numeric'
+                        placeholder='0.52%'
+                        value={this.state.lvl}
+                        onChangeText={text => this.setState({ lvl: text })}  />
+                </View>
+                <View>
+                    <FormLabel>Âge</FormLabel>
+                    <FormInput
+                        keyboardType='numeric'
+                        placeholder='35 ans'
+                        value={this.state.years}
+                        onChangeText={text => this.setState({ years: text })} />
+                </View>
+                <View>
+                    <Text style={{marginLeft: 20, marginTop: 20,}}>Sexe</Text>
                     <RadioButtons
                         options={options}
                         onSelection={setSelectedOption.bind(this)}
@@ -223,28 +243,33 @@ class AddBirdForm extends Component {
                         })}
                     />
                 </View>
-                <Field
-                    keyboardType='numeric'
-                    label='Âge'
-                    placeholder='35ans'
-                    value={this.state.years}
-                    onChangeText={text => this.setState({ years: text })} />
                 <View>
                     <Button
+                        raised
+                        icon={{ name: 'play', type: 'font-awesome' }}
                         title='Ajouter loiseau'
-                        onPress={this.onButtonPress.bind(this)}>
-                    </Button>
+                        onPress={this.onButtonPress.bind(this)}
+                        backgroundColor="#2095f3"
+                        containerViewStyle={styles.btn} />
                 </View>
                 <Text>
                     {this.state.message}
                     {this.state.error}
                 </Text>
-                <Text>
-                    {this.state.birds}
-                </Text>
             </ScrollView>
         )
     }
 }
+const styles = StyleSheet.create({
+    btn: {
+        marginTop: 20,
+    },
+    marginItem: {
+        marginBottom: 20,
+        marginTop: 20,
+        marginRight: 20,
+        marginLeft: 20,
+    }
+})
 
 export { AddBirdForm };
